8acc92d0b15ea8dd8a86a5ff03181b9b34826bea
SlashNephy <spica@starry.blue>
2023-02-25 20:57:32 +0900

    ポート変える

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 7070,
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
