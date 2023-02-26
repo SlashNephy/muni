cf48badfc4a015edddf79efe1593f2c06eec1e94
SlashNephy <spica@starry.blue>
2023-02-27 01:56:14 +0900

    gRPC Server Stream

---
+++ frontend/src/lib/useScreenWakeLock.ts
@@ -0,0 +1,32 @@
+import { useDocumentVisibility } from '@mantine/hooks'
+import { useEffect, useState } from 'react'
+import { useWakeLock } from 'react-screen-wake-lock'
+
+export const useScreenWakeLock = (
+  onVisible: () => void,
+  onHidden: () => void
+): void => {
+  const documentState = useDocumentVisibility()
+  const { isSupported, request, release } = useWakeLock()
+  const [wasRequested, setWasRequested] = useState(false)
+
+  useEffect(() => {
+    switch (documentState) {
+      case 'visible':
+        onVisible()
+
+        if (isSupported && !wasRequested) {
+          request('screen').catch(console.error)
+          setWasRequested(true)
+        }
+        return
+      case 'hidden':
+        onHidden()
+
+        if (isSupported && wasRequested) {
+          release().catch(console.error)
+          setWasRequested(false)
+        }
+    }
+  }, [documentState, isSupported, release, request])
+}
--- frontend/src/routes/floors/:floorId.tsx
+++ frontend/src/routes/floors/:floorId.tsx
@@ -1,35 +1,54 @@
 import { css } from '@emotion/react'
 import { AppShell, AspectRatio } from '@mantine/core'
-import { useDocumentVisibility } from '@mantine/hooks'
-import React, { useEffect, useRef, useState } from 'react'
+import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'react-router'
-import { useWakeLock } from 'react-screen-wake-lock'
+
+import { config } from '../../lib/config'
+import { useScreenWakeLock } from '../../lib/useScreenWakeLock'
+import { FloorServiceClient } from '../../pb/Floor.client'
 
 export const Floor: React.FC = () => {
   const { floorId } = useParams<'floorId'>()
 
-  const documentState = useDocumentVisibility()
-  const { isSupported, request, release } = useWakeLock()
   const videoRef = useRef<HTMLVideoElement>(null)
   const [videoUrl] = useState(
     'https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
   )
 
+  const client = useMemo(
+    () =>
+      new FloorServiceClient(
+        new GrpcWebFetchTransport({
+          format: 'binary',
+          baseUrl: config.backendOrigin,
+        })
+      ),
+    []
+  )
+
   useEffect(() => {
-    switch (documentState) {
-      case 'hidden':
-        videoRef.current?.pause()
-        if (isSupported) {
-          release().catch(console.error)
-        }
-        return
-      case 'visible':
-        void videoRef.current?.play()
-        if (isSupported) {
-          request('screen').catch(console.error)
-        }
+    const promise = async () => {
+      const p = client.joinFloor({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        floorId: floorId!,
+      })
+
+      for await (const res of p.responses) {
+        console.log(res)
+      }
     }
-  }, [documentState, isSupported, release, request])
+    promise().catch(console.error)
+  }, [client, floorId])
+
+  useScreenWakeLock(
+    () => {
+      void videoRef.current?.play()
+    },
+    () => {
+      videoRef.current?.pause()
+    }
+  )
 
   return (
     <AppShell>
